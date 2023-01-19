dae0b1a6b73dec3b4fc14471fd127bd5f2045a34
Mostafa <m.nasiri13@gmail.com>
2023-01-19 19:02:56 +0330

    make notification type field lowercase in response model

--- src/main/java/io/github/mostafanasiri/pansy/features/notification/presentation/response/NotificationType.java
+++ src/main/java/io/github/mostafanasiri/pansy/features/notification/presentation/response/NotificationType.java
@@ -1,7 +1,14 @@
 package io.github.mostafanasiri.pansy.features.notification.presentation.response;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum NotificationType {
     LIKE,
     COMMENT,
-    FOLLOW
+    FOLLOW;
+
+    @JsonValue
+    public String toLowerCase() {
+        return toString().toLowerCase();
+    }
 }
