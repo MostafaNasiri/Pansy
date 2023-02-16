496679b82cb761811af278a168c6d599cd22e27c
Mostafa <m.nasiri13@gmail.com>
2023-02-16 20:01:12 +0330

    FeedServiceTest - add tests for removePostFromFollowersFeeds

--- src/test/java/io/github/mostafanasiri/pansy/features/FeedServiceTest.java
+++ src/test/java/io/github/mostafanasiri/pansy/features/FeedServiceTest.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 
@@ -34,7 +35,7 @@ public class FeedServiceTest extends BaseServiceTest {
     private FeedService service;
 
     @Test
-    public void addPostToFollowersFeeds_successful_addsPostToFeeds() {
+    public void addPostToFollowersFeeds_successful_addsPost() {
         // Arrange
         var post = new Post(0, new User(1), null, null, 0, 0, null);
 
@@ -58,5 +59,58 @@ public class FeedServiceTest extends BaseServiceTest {
 
         // Assert
         assertEquals(feeds.get(0).getItems().size(), 1);
+    }
+
+    @Test
+    public void removePostFromFollowersFeeds_successful_removesPost() {
+        // Arrange
+        var post = new Post(0, new User(1), null, null, 0, 0, null);
+
+        List<Integer> followerIds = new ArrayList<>();
+        followerIds.add(1);
+
+        when(followerJpaRepository.getFollowersIds(post.getUser().id()))
+                .thenReturn(followerIds);
+
+        List<FeedEntity> feeds = new ArrayList<>();
+
+        var feed = new FeedEntity();
+        feed.getItems().add(new FeedEntity.FeedItem(post.getUser().id(), post.getId()));
+
+        feeds.add(feed);
+
+        when(feedJpaRepository.findAllById(followerIds))
+                .thenReturn(feeds);
+
+        // Act
+        service.removePostFromFollowersFeeds(post);
+
+        // Assert
+        assertEquals(feeds.get(0).getItems().size(), 0);
+    }
+
+    @Test
+    public void removePostFromFollowersFeeds_successful_updatesDatabase() {
+        // Arrange
+        var post = new Post(0, new User(1), null, null, 0, 0, null);
+
+        List<Integer> followerIds = new ArrayList<>();
+        followerIds.add(1);
+
+        when(followerJpaRepository.getFollowersIds(post.getUser().id()))
+                .thenReturn(followerIds);
+
+        List<FeedEntity> feeds = new ArrayList<>();
+        feeds.add(new FeedEntity());
+
+        when(feedJpaRepository.findAllById(followerIds))
+                .thenReturn(feeds);
+
+        // Act
+        service.removePostFromFollowersFeeds(post);
+
+        // Assert
+        verify(feedJpaRepository)
+                .saveAll(feeds);
     }
 }
