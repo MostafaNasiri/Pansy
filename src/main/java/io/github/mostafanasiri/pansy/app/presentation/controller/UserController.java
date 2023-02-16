eff6766020d24ab1f7ee88b406bc83e9317fd7f6
Mostafa <m.nasiri13@gmail.com>
2023-02-16 20:15:46 +0330

    merge NotificationController with UserController

--- src/main/java/io/github/mostafanasiri/pansy/app/presentation/controller/UserController.java
+++ src/main/java/io/github/mostafanasiri/pansy/app/presentation/controller/UserController.java
@@ -3,8 +3,10 @@ package io.github.mostafanasiri.pansy.app.presentation.controller;
 import io.github.mostafanasiri.pansy.app.common.ApiResponse;
 import io.github.mostafanasiri.pansy.app.domain.model.Image;
 import io.github.mostafanasiri.pansy.app.domain.model.User;
+import io.github.mostafanasiri.pansy.app.domain.service.NotificationService;
 import io.github.mostafanasiri.pansy.app.domain.service.PostService;
 import io.github.mostafanasiri.pansy.app.domain.service.UserService;
+import io.github.mostafanasiri.pansy.app.presentation.mapper.NotificationResponseMapper;
 import io.github.mostafanasiri.pansy.app.presentation.mapper.PostResponseMapper;
 import io.github.mostafanasiri.pansy.app.presentation.mapper.UserResponseMapper;
 import io.github.mostafanasiri.pansy.app.presentation.request.FollowUnfollowUserRequest;
@@ -12,6 +14,7 @@ import io.github.mostafanasiri.pansy.app.presentation.request.UpdateUserRequest;
 import io.github.mostafanasiri.pansy.app.presentation.response.FullUserResponse;
 import io.github.mostafanasiri.pansy.app.presentation.response.MinimalUserResponse;
 import io.github.mostafanasiri.pansy.app.presentation.response.PostResponse;
+import io.github.mostafanasiri.pansy.app.presentation.response.notification.NotificationResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -33,9 +36,14 @@ public class UserController {
     @Autowired
     private PostService postService;
     @Autowired
+    private NotificationService service;
+
+    @Autowired
     private UserResponseMapper userResponseMapper;
     @Autowired
     private PostResponseMapper postResponseMapper;
+    @Autowired
+    private NotificationResponseMapper notificationResponseMapper;
 
     @GetMapping("/")
     @Operation(summary = "Searches in users by their usernames and full names")
@@ -150,6 +158,26 @@ public class UserController {
         var result = posts.stream()
                 .map(postResponseMapper::mapFromPostModel)
                 .toList();
+
+        return new ResponseEntity<>(new ApiResponse<>(ApiResponse.Status.SUCCESS, result), HttpStatus.OK);
+    }
+
+    @GetMapping("/me/notifications")
+    @Operation(summary = "Returns a list of the authenticated user's notifications")
+    public ResponseEntity<ApiResponse<List<NotificationResponse>>> getNotifications(
+            @RequestParam(defaultValue = "0") @PositiveOrZero int page,
+            @RequestParam(defaultValue = "50") @Size(min = 1, max = 50) int size
+    ) {
+        var notifications = service.getNotifications(page, size);
+        var result = notificationResponseMapper.mapFromNotificationModels(notifications);
+
+        return new ResponseEntity<>(new ApiResponse<>(ApiResponse.Status.SUCCESS, result), HttpStatus.OK);
+    }
+
+    @GetMapping("/me/notifications/unread/count")
+    @Operation(summary = "Returns total number of the authenticated user's unread notifications")
+    public ResponseEntity<ApiResponse<Integer>> getUnreadNotificationCount() {
+        int result = service.getUnreadNotificationsCount();
 
         return new ResponseEntity<>(new ApiResponse<>(ApiResponse.Status.SUCCESS, result), HttpStatus.OK);
     }
